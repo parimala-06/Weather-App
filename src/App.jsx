f887a420cd239edd6117bbf9cddd94f4c77695ab
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-17 15:47:22 +0530

    Added container

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from "react"
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello world!
-    </h1>
+    <div className="flex flex-col bg-[url('assets/weather-app-img/images/bg2.jpg')] bg-contain justify-center items-center h-screen">
+      <div className="text-center font-medium font-sans text-3xl mb-4">Weather Application</div>
+      <div className=" backdrop-blur-sm bg-white/15 h-3/4 w-1/2 text-center rounded-xl">hi</div>
+
+
+    </div>
+
+ 
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg3': "url('assets/weather-app-img/images/bg3.jpg')",
+        'bg2': "url('my-project/src/assets/weather-app-img/images/bg2.jpg')",
+      },
+    },
   },
   plugins: [],
 }
