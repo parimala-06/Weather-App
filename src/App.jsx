2290b64ab65f0feb0ce3adebfa6ade75c2c58b43
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-18 23:54:48 +0530

    Fetched data using OpemWeather API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react"
+import dotenv from "dotenv"
 import Search from "./components/SearchBar.jsx"
 import Climate from "./components/Climate.jsx"
 import WindSpeed from "./components/WindSpeed.jsx"
 import Humidity from "./components/Humidity.jsx"
 
 function App() {
+
+  const apiKey = "54d164f876854d9769bc70c9ab52cde8"
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=coimbatore&units=metric"
+
+  async function weather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data)
+  }
+  weather();
 
   return (
     <div className="flex flex-col bg-[url('assets/images/bg2.jpg')] bg-contain items-center h-screen">
