6d443251e709560bd3fbedf4adfbeeb4b5ec13b2
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-19 14:04:52 +0530

    Included images

--- src/components/Climate.jsx
+++ src/components/Climate.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import imgurl from '../assets/images'
+import cloudsImage  from '../assets/images/clouds.png'
 
 const Climate = (props) => {
   return (
     <div className="flex flex-col justify-center items-center h-1/2 mt-5 gap-1 mb-3">
-        <img className="rounded-full w-36 h-36" src={props.image?`${imgurl}/${props.image}.png`:`${imgurl}/clouds.png`} alt="image description"/>
+        <img className="rounded-full w-36 h-36" src={props.image?require(`../assets/images/${props.image}.png`) : cloudsImage} alt="image description"/>
         <div className="text-lg">{props.place?props.place:`Place`}</div>
         <div className="text-3xl">{props.temp?props.temp:`0`} °C</div>
         <div className="text-lg">{props.description?props.description:`Description`}</div>
