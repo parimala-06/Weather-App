39ae27413a38bae521f15aea5a7402f7f442b3cc
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-19 13:18:30 +0530

    Change image according to weather description

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
   const [windSpeed, setWindSpeed] = useState("");
   const [description, setDescription] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+  const [image, setImage] = useState("");
+
 
   const apiKey = "54d164f876854d9769bc70c9ab52cde8";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}&q=${searchQuery}`;
@@ -27,6 +29,25 @@ function App() {
       setHumidity(data.main.humidity.toFixed(1));
       setWindSpeed(data.wind.speed.toFixed(1));
       setDescription(data.weather[0].main);
+
+      if (data.weather[0].main == "Clouds"){
+        setImage("src/assets/images/clouds.png");
+      }
+      else if(data.weather[0].main == "Clear"){
+        setImage("src/assets/images/clear.png");
+      }
+      else if(data.weather[0].main == "Drizzle"){
+        setImage("src/assets/images/drizzle.png");
+      }
+      else if(data.weather[0].main == "Rain"){
+        setImage("src/assets/images/rain.png");
+      }
+      else if(data.weather[0].main == "Thunderstrom"){
+        setImage("src/assets/images/thunderstrom.png");
+      }
+      else{
+        setImage("src/assets/images/clouds.png");
+      }
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
@@ -41,7 +62,7 @@ function App() {
       <div className="text-center font-medium font-sans text-3xl m-4">Weather Application</div>
       <Search onChange={handleSearchInputChange} onSearch={handleSearch} />
       <div className="flex flex-col gap-8 backdrop-blur-sm bg-white/10 h-1/2 w-1/4 text-center rounded-xl">
-        <Climate place={placeName} temp={temp} description={description}/>
+        <Climate place={placeName} temp={temp} description={description} image={image}/>
         <div className="flex justify-around m-5 p-2 gap-5">
           <WindSpeed windSpeed = {windSpeed}/>
           <Humidity humidity={humidity}/>
