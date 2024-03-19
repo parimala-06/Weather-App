19a5528d488355500acce21ed64ea4b8db7bc3a9
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-19 14:18:36 +0530

    Altered Image path

--- src/components/Climate.jsx
+++ src/components/Climate.jsx
@@ -4,7 +4,7 @@ import cloudsImage  from '../assets/images/clouds.png'
 const Climate = (props) => {
   return (
     <div className="flex flex-col justify-center items-center h-1/2 mt-5 gap-1 mb-3">
-        <img className="rounded-full w-36 h-36" src={props.image? `src/assets/images/${props.image}.png` : cloudsImage} alt="image description"/>
+        <img className="rounded-full w-36 h-36" src={props.image? `../src/assets/images/${props.image}.png` : cloudsImage} alt="image description"/>
         <div className="text-lg">{props.place?props.place:`Place`}</div>
         <div className="text-3xl">{props.temp?props.temp:`0`} °C</div>
         <div className="text-lg">{props.description?props.description:`Description`}</div>
